1ed5dbd0d59dab90573323e37fa72f5516171452
cq179697 <bondook714@yahoo.com>
2016-09-29 11:14:46 -0500

    Add files via upload

---
+++ Do While Demo/Do While Demo/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+
+    Private Sub btnRunDemo_Click(sender As Object, e As EventArgs) Handles btnRunDemo.Click
+        Dim intCount As Integer = 0
+
+        Do While intCount < 10
+            lstOutput.Items.Add("Hello")
+            intCount += 1
+        Loop
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+
+    End Sub
+End Class
